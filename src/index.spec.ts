150c69f8d939e8c68c6b31f8a558b5afffa6dbc0
= <samualdjpayne@hotmail.com>
2019-06-16 18:25:29 +0100

    Applied hotfix for linting

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -7,15 +7,15 @@ import { greeting } from './index';
 
 describe('Greeting', () => {
   it('should interpolate strings correctly', () => {
-    const input: string = 'Sam';
-    const expectedOutput: string = 'Hello, Sam!';
-    const result: string = greeting(input);
+    const input : string = 'Sam';
+    const expectedOutput : string = 'Hello, Sam!';
+    const result : string = greeting(input);
     expect(result).toEqual(expectedOutput);
   });
 
   it('should use a default value if no args specified', () => {
-    const expectedOutput: string = 'Hello, World!';
-    const result: string = greeting();
+    const expectedOutput : string = 'Hello, World!';
+    const result : string = greeting();
     expect(result).toEqual(expectedOutput);
   });
 });
